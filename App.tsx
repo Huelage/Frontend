cb5e9a2f9166c6812ffd0ee0342951d4ca6afa3d
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-11 16:39:08 +0100

    Remove unused fonts declaration

--- App.tsx
+++ App.tsx
@@ -1,22 +1,16 @@
 import {
-  BerkshireSwash_400Regular,
-  InterTight_300Light_Italic,
   Inter_200ExtraLight,
+  InterTight_200ExtraLight_Italic,
   Inter_300Light,
+  InterTight_300Light_Italic,
   Inter_400Regular,
+  InterTight_400Regular_Italic,
   Inter_500Medium,
+  InterTight_500Medium_Italic,
   Inter_600SemiBold,
+  InterTight_600SemiBold_Italic,
   Inter_700Bold,
-  OpenSans_300Light,
-  OpenSans_300Light_Italic,
-  OpenSans_400Regular,
-  OpenSans_400Regular_Italic,
-  OpenSans_500Medium,
-  OpenSans_500Medium_Italic,
-  OpenSans_600SemiBold,
-  OpenSans_600SemiBold_Italic,
-  OpenSans_700Bold,
-  OpenSans_700Bold_Italic,
+  InterTight_700Bold_Italic,
   useFonts
 } from '@expo-google-fonts/dev';
 import { NavigationContainer } from "@react-navigation/native";
@@ -27,27 +21,20 @@ import TabNavigator from './src/navigators/TabNavigator';
 
 const App = () => {
   const [fontsLoaded] = useFonts({
-    Cooper_Black_Regular: require('./assets/fonts/Cooper_Black_Regular.ttf'),
     Inter_200ExtraLight,
+    InterTight_200ExtraLight_Italic,
     Inter_300Light,
     InterTight_300Light_Italic,
     Inter_400Regular,
+    InterTight_400Regular_Italic,
     Inter_500Medium,
+    InterTight_500Medium_Italic,
     Inter_600SemiBold,
+    InterTight_600SemiBold_Italic,
     Inter_700Bold,
-    BerkshireSwash_400Regular,
-    OpenSans_300Light,
-    OpenSans_300Light_Italic,
-    OpenSans_400Regular,
-    OpenSans_400Regular_Italic,
-    OpenSans_500Medium,
-    OpenSans_500Medium_Italic,
-    OpenSans_600SemiBold,
-    OpenSans_600SemiBold_Italic,
-    OpenSans_700Bold,
-    OpenSans_700Bold_Italic
+    InterTight_700Bold_Italic
   });
-  const [isSignedIn, setIsSignedIn] = useState<boolean>(true);
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
 
   if (!fontsLoaded) return null;
   return (
