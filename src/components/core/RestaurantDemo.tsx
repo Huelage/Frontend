21b113036cf6f9dc0ed129397cc4c0d85f84820d
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-12 09:26:10 +0100

    Add a new component that helps render each individual restaturanct card on the Home screen in the Favourite restaurant section

---
+++ src/components/core/RestaurantDemo.tsx
@@ -0,0 +1,70 @@
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { shadowStyle } from '../../utils';
+import { fonts } from '../../utils/fontEnum';
+import { RestaurantInterface } from '../../utils/interfaces';
+
+interface RestaurantProps extends RestaurantInterface {
+  addToCart: () => void;
+}
+
+const RestaurantDemo = ({ name, imgUrl, rating, location, addToCart }: RestaurantProps) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.resImage} source={{ uri: imgUrl }} />
+      <View style={styles.detailsBox}>
+        <Text style={styles.resName}>{name}</Text>
+        <View style={styles.resRatingBox}>
+          <Ionicons name="star" size={16} color="#F2DB06" />
+          <Ionicons name="star" size={16} color="#F2DB06" />
+          <Ionicons name="star" size={16} color="#F2DB06" />
+          <Ionicons name="star" size={16} color="#F2DB06" />
+          <Ionicons name="star" size={16} color="#F2DB06" />
+        </View>
+        <Text style={styles.resLocation}>{location}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default RestaurantDemo;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F0FFF0',
+    borderRadius: 15,
+    gap: 5,
+    minHeight: hp("30%"),
+    width: wp("60%"),
+    ...shadowStyle
+  },
+  resImage: {
+    borderRadius: 15,
+    height: 130,
+    width: "100%"
+  },
+  detailsBox: {
+    gap: 5,
+    padding: 10
+  },
+  resName: {
+    fontFamily: fonts.I_700,
+    fontSize: 14,
+    textAlign: 'center'
+  },
+  resRatingBox: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 3,
+    justifyContent: 'center'
+  },
+  resLocation: {
+    color: '#38742A',
+    fontFamily: fonts.I_500,
+    fontSize: 16,
+    textAlign: 'center'
+  }
+});
