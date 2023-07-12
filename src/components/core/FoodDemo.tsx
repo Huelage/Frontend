4cab5aee8fa9b50058a993943e0e17fa0e651a21
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-12 01:27:47 +0100

    Update the FoodDemo component to reflect the new UI changes

--- src/components/core/FoodDemo.tsx
+++ src/components/core/FoodDemo.tsx
@@ -1,27 +1,44 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { CheckBox } from '@rneui/themed';
 import React from 'react';
-import { FoodInterface } from '../../utils/interfaces';
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { shadowStyle } from '../../utils';
 import { fonts } from '../../utils/fontEnum';
-import CustomButton from './CustomButton';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FoodInterface } from '../../utils/interfaces';
 
 interface FoodProps extends FoodInterface {
   addToCart: () => void;
 }
-const FoodDemo = ({ imgUrl, name, price, addToCart }: FoodProps) => {
+
+const FoodDemo = ({ imgUrl, name, price, rating, cals, addToCart }: FoodProps) => {
   console.log(imgUrl);
   return (
     <View style={styles.container}>
-      <Image style={styles.foodImage} source={require('../../../assets/images/prawnImg.png')} />
-      <Text style={styles.foodName}>{name}</Text>
-      <View style={styles.foodAction}>
-        <Text style={styles.foodPrice}>
-          <MaterialCommunityIcons name="currency-ngn" size={18} color="black" />
-          {price}
-        </Text>
-        <CustomButton fontSize={14} height={40} label='Add' icon="add-circle-outline" onPress={addToCart} />
+      <View style={styles.foodIntro}>
+        <View style={styles.foodRatingBox}>
+          <Ionicons name="star" size={24} color="#47CA4C" />
+          <Text style={styles.foodRating}>{rating}</Text>
+        </View>
+        <Image style={styles.foodImage} source={require('../../../assets/images/prawnImg.png')} />
+        <CheckBox
+          checked={true}
+          onPress={() => { }}
+          checkedIcon="heart"
+          uncheckedIcon="heart-o"
+          checkedColor="red"
+          containerStyle={{ backgroundColor: '#F0FFF0', padding: 0, margin: 0 }}
+        />
       </View>
+      <Text style={styles.foodName}>{name}</Text>
+      <View style={styles.foodCalories}>
+        <MaterialCommunityIcons name="fire" size={22} color="#47CA4C" />
+        <Text style={styles.foodCaloriesText}>{cals} KCal</Text>
+      </View>
+      <Text style={styles.foodPrice}>
+        <MaterialCommunityIcons name="currency-ngn" size={12} color="black" />
+        {price}
+      </Text>
     </View>
   );
 };
@@ -31,21 +48,47 @@ export default FoodDemo;
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    borderColor: '#4CAF50',
-    borderRadius: 20,
-    borderWidth: 2,
-    height: wp("60%"),
+    backgroundColor: '#F0FFF0',
+    borderRadius: 10,
     justifyContent: 'center',
-    padding: 15,
-    width: wp("60%")
+    padding: 10,
+    gap: 5,
+    width: wp("60%"),
+    marginTop: 80,
+    ...shadowStyle
+  },
+  foodIntro: {
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    gap: 5,
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 35
   },
   foodImage: {
-    height: 130,
-    width: 130
+    position: 'absolute',
+    left: 40,
+    top: -hp("10%"),
+    height: 150,
+    width: 150,
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: .3,
+    shadowRadius: 5,
+    zIndex: 2
+  },
+  foodCalories: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 3,
+    justifyContent: 'center'
+  },
+  foodCaloriesText: {
+    fontFamily: fonts.I_400,
+    fontSize: 12
   },
   foodName: {
-    fontFamily: fonts.O_600,
-    fontSize: 18,
+    fontFamily: fonts.I_700,
+    fontSize: 14,
     textAlign: 'center'
   },
   foodAction: {
@@ -58,7 +101,18 @@ const styles = StyleSheet.create({
   foodPrice: {
     alignItems: 'center',
     justifyContent: 'center',
-    fontFamily: fonts.O_600,
-    fontSize: 18
+    fontFamily: fonts.I_500,
+    fontSize: 14
+  },
+  foodRatingBox: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 2,
+    justifyContent: 'center'
+  },
+  foodRating: {
+    fontFamily: fonts.I_400,
+    fontSize: 14,
+    paddingTop: 6
   }
 });
