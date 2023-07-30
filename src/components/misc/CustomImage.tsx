f0bb19ad5f9a5b8913790f66fae0f7ab20110634
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-31 00:05:49 +0100

    HACO-19 Add new optional props to the custom image component

--- src/components/misc/CustomImage.tsx
+++ src/components/misc/CustomImage.tsx
@@ -1,4 +1,4 @@
-import { Canvas, Group, Image, Shadow, rect, rrect, useImage } from '@shopify/react-native-skia';
+import { Canvas, Fit, Group, Image, Shadow, rect, rrect, useImage } from '@shopify/react-native-skia';
 import React from 'react';
 import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
 
@@ -6,18 +6,22 @@ interface ImageInterface {
   imgUrl: string;
   imgSize: number;
   imgPad: number;
+  imgFit?: Fit;
+  shadowBlur?: number;
   shadowColor?: string;
+  shadowHeight?: number;
+  shadowWidth?: number;
   style: StyleProp<ViewStyle>;
 }
 
-const CustomImage = ({ imgUrl, imgSize, imgPad, style, shadowColor }: ImageInterface) => {
+const CustomImage = ({ imgUrl, imgSize, imgPad, imgFit, style, shadowBlur, shadowColor, shadowHeight, shadowWidth }: ImageInterface) => {
   const img = useImage(imgUrl);
   const roundedRect = rrect(rect(imgPad, imgPad, imgSize, imgSize), 1000, 1000);
   return (
     <Canvas style={style}>
-      <Shadow dx={0} dy={6} blur={5} color={shadowColor || "rgba(0, 0, 0, .4)"} />
+      <Shadow dx={shadowWidth || 0} dy={shadowHeight || 6} blur={shadowBlur || 5} color={shadowColor || "rgba(0, 0, 0, .4)"} />
       <Group clip={roundedRect}>
-        <Image image={img} x={imgPad} y={imgPad} width={imgSize} height={imgSize} fit="cover" />
+        <Image image={img} x={imgPad} y={imgPad} width={imgSize} height={imgSize} fit={imgFit || "cover"} />
       </Group>
     </Canvas>
   );
