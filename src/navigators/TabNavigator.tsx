3adf0e071ee948195e70bad61599885d5b984f83
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-15 15:32:39 +0100

    HACO-33 Update the screen names and navigation props for the user side tabs

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,13 +1,7 @@
 import { TabBar, TabHeader } from "@components/navigation";
 import { TabParamList } from "@interfaces";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {
-  FavouriteScreen,
-  HomeScreen,
-  MenuScreen,
-  ProfileScreen,
-  CartScreen,
-} from "@screens/core";
+import { HistoryScreen, HomeScreen, ProfileScreen, VendorScreen } from "@screens/core";
 import React from "react";
 import { View } from "react-native";
 
@@ -24,8 +18,8 @@ const TabNavigator = () => {
       tabBar={(props) => <TabBar {...props} />}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Vendors" component={MenuScreen} />
-      <Tab.Screen name="History" component={FavouriteScreen} />
+      <Tab.Screen name="Vendors" component={VendorScreen} />
+      <Tab.Screen name="History" component={HistoryScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
