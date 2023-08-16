3dfb9e84ad049a07e60e0f1b16f829da5883b998
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-16 16:34:23 +0100

    HACO-35 Add section components for the overview section and the order summary section to the vendor flow home screen

--- src/screens/Vendors/HomeScreen.tsx
+++ src/screens/Vendors/HomeScreen.tsx
@@ -2,12 +2,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { useAppTheme } from '@hooks';
 import { OrderOverview } from '@components/vendor/Home';
+import { OrderSummary } from '@containers/Vendor';
 
 const HomeScreen = () => {
   const { color } = useAppTheme();
   return (
     <View style={[styles.container, { backgroundColor: color.mainBg }]}>
       <OrderOverview />
+      <OrderSummary />
     </View>
   );
 };
@@ -17,7 +19,7 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: 10,
+    gap: 30,
     padding: 20
   }
 });
