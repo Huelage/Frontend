d162a38ca4430878ce44130d76facdd2cf7b0571
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-16 11:24:05 +0100

    HACO-34 Add the Vendor flow stack navigator

---
+++ src/navigators/VendorStackNavigator.tsx
@@ -0,0 +1,18 @@
+import { VendorStackParamList } from '@interfaces';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import VendorTabNavigator from './VendorTabNavigator';
+import { NotificationScreen } from '@screens/Vendors';
+
+const Stack = createNativeStackNavigator<VendorStackParamList>();
+
+const VendorStackNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName='MainTabs' screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="MainTabs" component={VendorTabNavigator} />
+      <Stack.Screen name="Notifications" component={NotificationScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default VendorStackNavigator;
