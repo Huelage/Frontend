b5083e0756290a7659f0aba274033b8a4634cccf
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-16 16:55:41 +0100

    HACO-35 Implement auto dark mode toggle using system preferences on app start-up

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -1,15 +1,21 @@
-import { useAppSelector } from '@api/app/appHooks';
-import { getAuthStatus } from '@api/slices/globalSlice';
+import { useAppDispatch, useAppSelector } from '@api/app/appHooks';
+import { getAuthStatus, switchTheme } from '@api/slices/globalSlice';
 import { useAppTheme } from '@hooks';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AuthStackNavigator from './AuthStackNavigator';
 import StackNavigator from './StackNavigator';
+import { useColorScheme } from 'react-native';
 
 const MainNavigator = () => {
+  const dispatch = useAppDispatch();
   const isSignedIn = useAppSelector(getAuthStatus);
   const { theme } = useAppTheme();
+  const nativeTheme = useColorScheme();
+  useEffect(() => {
+    dispatch(switchTheme(nativeTheme || "light"));
+  }, []);
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer theme={theme === 'dark' ? DarkTheme : DefaultTheme}>
