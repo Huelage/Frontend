5aecc5cac3ecd799dde00341cf9c7dc0c5731372
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-16 11:40:14 +0100

    HACO-34 Clean up and update the navigators import statements and Repurpose the StackNavigator to a conditional flow decisider for the user or vendor flow

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -1,14 +1,14 @@
 import { useAppSelector } from '@api/app/appHooks';
-import { isAuthenticated } from '@api/slices/globalSlice';
+import { getAuthStatus } from '@api/slices/globalSlice';
+import { useAppTheme } from '@hooks';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import StackNavigator from './StackNavigator';
-import AuthStackNavigator from './AuthStackNavigator';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useAppTheme } from '@hooks';
+import AuthStackNavigator from './AuthStackNavigator';
+import StackNavigator from './StackNavigator';
 
 const MainNavigator = () => {
-  const isSignedIn = useAppSelector(isAuthenticated);
+  const isSignedIn = useAppSelector(getAuthStatus);
   const { theme } = useAppTheme();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
