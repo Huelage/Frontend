6a5465980586bf54a22bea282267f171a6001480
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-29 11:14:50 +0100

    HACO-38 Add a new login screen to create the new UI while leaving the old ui for reference or backup

--- src/screens/auth/index.ts
+++ src/screens/auth/index.ts
@@ -1,4 +1,4 @@
-export { default as LoginScreen } from './LoginScreen';
+export { default as LoginScreen } from './LoginScreen2';
 export { default as OTPScreen } from './OTPScreen';
 export { default as OnBoardScreen } from './OnBoardScreen';
 export { default as SignUpScreen } from './SignUpScreen';
