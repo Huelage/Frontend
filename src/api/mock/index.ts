30bd0e2264c71c50bc65ff6aed1330a842c76895
Toby2507 <oluwatobisalau2000@gmail.com>
2023-08-30 22:28:59 +0100

    HACO-38 Update the UI for the login screen using the LoginScreen2 which is renamed to LoginScreen

--- src/api/mock/index.ts
+++ src/api/mock/index.ts
@@ -2,3 +2,4 @@ export { default as mockCategories } from "./mockCategoryData";
 export { default as mockFoods } from "./mockFoodData";
 export { default as mockRestaurants } from "./mockRestaurants";
 export { default as mockCartItem } from "./mockCartItem";
+export { default as mockReviews } from "./mockVendorReview";
