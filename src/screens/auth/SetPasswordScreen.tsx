8b1d6c60912beab07c742e30c32cebc21333fc81
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-16 00:59:48 +0100

    HACO-41 Renamed change password component to set password, Refactor it to support theme switching and prepare it for unit testing

---
+++ src/screens/auth/SetPasswordScreen.tsx
@@ -0,0 +1,167 @@
+import { useAppDispatch, useAppSelector } from "@api/app/appHooks";
+import { getAuthStatus, getVendorStatus, setAuthStatus } from "@api/slices/globalSlice";
+import { CustomTextInput, SubmitButton } from "@components/auth";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useAppTheme } from "@hooks";
+import { AuthNavigationProps, ResetPasswordInterface } from "@interfaces";
+import { useNavigation } from "@react-navigation/native";
+import { fonts } from "@utils";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Keyboard, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Animated from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const SetPasswordScreen = () => {
+  const insets = useSafeAreaInsets();
+  const dispatch = useAppDispatch();
+  const { color } = useAppTheme();
+  const isSignedIn = useAppSelector(getAuthStatus);
+  const { goBack } = useNavigation<AuthNavigationProps>();
+  const { handleSubmit, control, setFocus, watch, reset, formState: { errors } } = useForm<ResetPasswordInterface>({ mode: "onChange" });
+  const onSubmit = (data: ResetPasswordInterface) => {
+    reset();
+    if (!isSignedIn) {
+      dispatch(setAuthStatus(true));
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => setFocus(isSignedIn ? "oldPassword" : "password"), 0);
+  }, []);
+  return (
+    <>
+      <StatusBar style="auto" />
+      <View style={[styles.container, { paddingTop: insets.top + 10 }]} onTouchStart={() => Keyboard.dismiss()} testID="set password screen">
+        <View style={styles.headerBox}>
+          <TouchableOpacity style={styles.backButton} onPress={goBack} testID="go back">
+            <AntDesign name="arrowleft" size={26} color={color.mainText} />
+          </TouchableOpacity>
+          <Text style={[styles.headerText, { color: color.mainText }]}>Set Password</Text>
+        </View>
+        <View style={styles.mainBox}>
+          <Animated.View sharedTransitionTag="reset password icons" style={[styles.iconWrap, { backgroundColor: color.mainGreen }]} testID="screen icon">
+            <MaterialCommunityIcons name="lock-open-plus-outline" size={100} color="white" />
+          </Animated.View>
+          <Text style={[styles.infoText, { color: color.mainText }]}>Your new password must be different from previously used password</Text>
+          {isSignedIn && (
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <CustomTextInput
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  error={errors.password}
+                  isPass={true}
+                  label="Old password"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  onSubmitEditing={() => setFocus("password")}
+                  innerRef={ref}
+                  returnKeyType="next"
+                  value={value}
+                />
+              )}
+              name="oldPassword"
+              rules={{ required: "Old Password is required" }}
+            />
+          )}
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+              <CustomTextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                error={errors.password}
+                isPass={true}
+                label="New password"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                onSubmitEditing={() => setFocus("confirmPassword")}
+                innerRef={ref}
+                returnKeyType="next"
+                value={value}
+              />
+            )}
+            name="password"
+            rules={{
+              required: "Password is required",
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{6,24}$/,
+                message:
+                  "• 6 to 24 characters.\n• Must include uppercase and lowercase letters, a number and a special character.\n• Allowed special characters: !@#$%^&*",
+              },
+            }}
+          />
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+              <CustomTextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                error={errors.confirmPassword}
+                isPass={true}
+                innerRef={ref}
+                label="Confirm Password"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                onSubmitEditing={handleSubmit(onSubmit)}
+                returnKeyType="done"
+                value={value}
+              />
+            )}
+            name="confirmPassword"
+            rules={{
+              required: "Confirm Password is required",
+              validate: value => value === watch("password") || "Passwords do not match"
+            }}
+          />
+          <SubmitButton label="Change Password" onSubmit={handleSubmit(onSubmit)} />
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default SetPasswordScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerBox: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  backButton: {
+    position: "absolute",
+    left: 20
+  },
+  headerText: {
+    fontFamily: fonts.I_600,
+    fontSize: 20
+  },
+  mainBox: {
+    flex: 1,
+    gap: 30,
+    paddingHorizontal: 20,
+    paddingTop: 70
+  },
+  iconWrap: {
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 100,
+    height: 160,
+    justifyContent: "center",
+    marginBottom: 30,
+    width: 160
+  },
+  infoText: {
+    fontFamily: fonts.I_500,
+    fontSize: 14,
+    textAlign: "center",
+  }
+});
