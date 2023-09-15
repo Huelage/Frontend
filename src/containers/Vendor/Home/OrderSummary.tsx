15fa279dfc75b3b1e39ee9514c38de64be7c3009
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-15 19:03:40 +0100

    HACO-44 Prepare for unit testing by adding testIDs

--- src/containers/Vendor/Home/OrderSummary.tsx
+++ src/containers/Vendor/Home/OrderSummary.tsx
@@ -1,9 +1,9 @@
-import { OrderChartElement, OverviewElement } from '@components/vendor/Home';
+import { OrderChartElement, SummaryElement } from '@components/vendor/Home';
 import { FontAwesome } from '@expo/vector-icons';
 import { useAppTheme } from '@hooks';
 import { fonts, shadowStyle } from '@utils';
 import React from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const OrderSummary = () => {
   const { color } = useAppTheme();
@@ -14,7 +14,7 @@ const OrderSummary = () => {
   ];
   const total = summary.reduce((acc, cur) => acc + cur.value, 0);
   return (
-    <View style={styles.container}>
+    <View style={styles.container} testID='order summary'>
       <View style={styles.header}>
         <Text style={[styles.title, { color: color.mainText }]}>Order Summary</Text>
         <TouchableOpacity style={[styles.filterButton, { backgroundColor: color.filterBg }]}>
@@ -22,12 +22,12 @@ const OrderSummary = () => {
           <FontAwesome name="angle-down" size={18} color={color.mainText} />
         </TouchableOpacity>
       </View>
-      <View style={styles.overviewBox}>
+      <View style={styles.overviewBox} testID='summary elements'>
         {summary.map((item, idx) => (
-          <OverviewElement key={idx} value={item.value} label={item.label} iconColor={item.iconColor} />
+          <SummaryElement key={idx} value={item.value} label={item.label} iconColor={item.iconColor} />
         ))}
       </View>
-      <View style={{ gap: 10 }}>
+      <View style={{ gap: 10 }} testID='chart elements'>
         {summary.map((item, idx) => (
           <OrderChartElement key={idx} idx={idx} iconColor={item.iconColor} value={item.value} label={item.label} total={total} />
         ))}
