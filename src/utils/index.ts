9e70cb210f9f992b62844ac952e2851b921b708f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-15 18:38:57 +0100

    HACO-44 Optimise and prepare the utils for unit testing

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ export { fonts } from './fontEnum';
 export { shadowStyle, outline } from './style';
 export { withAnchorPoint } from './anchorPoints';
 export { getBiometrics, authenticate, enableBiometrics, loginWithBiometrics, BiometricType } from './biometrics';
-export { FingerPrint, FaceId, Iris } from './svgs';
-export { CustomSecureStore } from './keysandstores';
+export { FingerPrint, FaceId, Iris, TotalCustomers, TotalMenu, TotalOrders } from './svgs';
+export { CustomSecureStore, replacer } from './keysandstores';
