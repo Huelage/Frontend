9669df62f19d90f6f88049a69fcaf6d852f15ed8
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-15 19:36:55 +0100

    HACO-44 Remove the SafeAreaProvider from the App.tsx

--- src/screens/Vendors/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AccountScreen } from './AccountScreen';
-export { default as HomeScreen } from './HomeScreen';
-export { default as MenuScreen } from './MenuScreen';
-export { default as OrderScreen } from './OrderScreen';
-export { default as NotificationScreen } from './NotificationScreen';
