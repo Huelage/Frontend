ebef471776a40e8d2e0480bf3e1bc9ddc95236ff
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-16 20:24:19 +0100

    HACO-51 Add a test ID to the screen skeletons to facilitate unit testing the new stack navigators for the user flow

--- src/screens/core/ProfileScreen.tsx
+++ src/screens/core/ProfileScreen.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const ProfileScreen = () => {
   return (
-    <View>
+    <View testID='profile screen'>
       <Text>ProfileScreen</Text>
     </View>
   );
