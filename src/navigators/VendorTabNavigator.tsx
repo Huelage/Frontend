ca30e2db1cc3df61b5544a05ea988275c73cad7b
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-16 20:20:55 +0100

    HACO-51 Update the tab navigators to use the new nested stacks

--- src/navigators/VendorTabNavigator.tsx
+++ src/navigators/VendorTabNavigator.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { VendorTabParamList } from '@interfaces';
-import { AccountScreen, HomeScreen, MenuScreen, OrderScreen } from '@screens/Vendor';
 import { VendorTabBar, VendorTabHeader } from '@components/navigation';
 import { useAppTheme } from '@hooks';
+import { VendorTabParamList } from '@interfaces';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from '@screens/Vendor';
+import React from 'react';
+import { View } from 'react-native';
+import { VendorAccountTabStack, VendorMenuTabStack, VendorOrdersTabStack } from './VendorTabStacks';
 
 const Tab = createBottomTabNavigator<VendorTabParamList>();
 
@@ -14,19 +15,16 @@ const VendorTabNavigator = () => {
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={{
-        header: () => <VendorTabHeader />,
         tabBarBackground: () => <View style={{ backgroundColor: color.mainBg, flex: 1 }} />
       }}
       tabBar={(props) => <VendorTabBar {...props} />}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Orders" component={OrderScreen} />
-      <Tab.Screen name="Menu" component={MenuScreen} />
-      <Tab.Screen name="Account" component={AccountScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ header: () => <VendorTabHeader /> }} />
+      <Tab.Screen name="Orders" component={VendorOrdersTabStack} options={{ headerShown: false }} />
+      <Tab.Screen name="Menu" component={VendorMenuTabStack} options={{ headerShown: false }} />
+      <Tab.Screen name="Account" component={VendorAccountTabStack} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 };
 
 export default VendorTabNavigator;
-
-const styles = StyleSheet.create({});
