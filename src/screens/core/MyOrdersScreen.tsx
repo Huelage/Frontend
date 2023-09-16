712351a832f27bf0716924439f0cf02f7a5a0ad9
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-16 20:22:30 +0100

    HACO-51 Add the skeleton for the my orders screen

---
+++ src/screens/core/MyOrdersScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MyOrdersScreen = () => {
+  return (
+    <View style={styles.container} testID='my orders screen'>
+      <Text>MyOrdersScreen</Text>
+    </View>
+  );
+};
+
+export default MyOrdersScreen;
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
