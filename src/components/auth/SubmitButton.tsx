d7339e62c433d7713d5852aeeb33cd8c6fdc97d0
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-24 10:50:08 +0100

    HACO-48 Add an optional prop to the submit button component which when supplied, displays and activity indicator

--- src/components/auth/SubmitButton.tsx
+++ src/components/auth/SubmitButton.tsx
@@ -1,19 +1,24 @@
 import { useAppTheme } from '@hooks';
 import { fonts, shadowStyle } from '@utils';
 import React, { BaseSyntheticEvent } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 interface SubmitProps {
 	label: string;
+	isLoading?: boolean;
 	onSubmit: ((e?: BaseSyntheticEvent<object, any, any> | undefined) => Promise<void>) | (() => void);
 }
 
-const SubmitButton = ({ label, onSubmit }: SubmitProps) => {
+const SubmitButton = ({ label, isLoading, onSubmit }: SubmitProps) => {
 	const { color } = useAppTheme();
 	return (
 		<TouchableOpacity onPress={onSubmit} testID='submit button'>
 			<View style={[styles.loginButton, { backgroundColor: color.mainGreen }]}>
-				<Text style={styles.loginText}>{label}</Text>
+				{isLoading ? (
+					<ActivityIndicator testID='loader' />
+				) : (
+					<Text style={styles.loginText}>{label}</Text>
+				)}
 			</View>
 		</TouchableOpacity>
 	);
