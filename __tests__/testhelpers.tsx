4acb662a9437eb3c9f05c0726362e5e90b9a1eca
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-28 10:01:07 +0100

    HACO-48 Add custom render function for components that uses the apolloclient

--- __tests__/testhelpers.tsx
+++ __tests__/testhelpers.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { configureStore } from "@reduxjs/toolkit";
 import { render } from "@testing-library/react-native";
 import { Provider } from "react-redux";
+import { MockedProvider } from "@apollo/client/testing";
 
 export const store = configureStore({
   reducer: globalSlice
@@ -16,13 +17,31 @@ export const renderNavigator = (ui: React.ReactNode) => (
   )
 );
 
-export const renderRedux = (ui: React.ReactNode) => (
+export const renderApollo = (ui: React.ReactNode, mocks: any) => {
   render(
-    <Provider store={store}>
+    <MockedProvider mocks={mocks} addTypename={false}>
       {ui}
-    </Provider>
-  )
-);
+    </MockedProvider>
+  );
+};
+
+export const renderApolloNavigator = (ui: React.ReactNode, mocks: any) => {
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <NavigationContainer>
+        {ui}
+      </NavigationContainer>
+    </MockedProvider>
+  );
+};
+
+// export const renderRedux = (ui: React.ReactNode) => (
+//   render(
+//     <Provider store={store}>
+//       {ui}
+//     </Provider>
+//   )
+// );
 
 const rect = { x: 0, y: 0, width: 0, height: 0 };
 export const testRect = { rect, rx: 0, ry: 0 };
