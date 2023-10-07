f8c526c5648281b02bd5f1975275f06f851a8236
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-07 17:33:29 +0100

    HACO-47 Update the mock food data to reflect the food interface

--- src/api/mock/mockFoodData.ts
+++ src/api/mock/mockFoodData.ts
@@ -1,115 +1,120 @@
+import { UserFoodInterface } from '@interfaces';
 import { Image } from 'react-native';
 
-const food = [
+const food: UserFoodInterface[] = [
   {
     id: "1",
-    name: "River prawn spicy soap",
-    desc: "River prawn spiacy soup eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
-    price: 1000,
-    imgUrl: Image.resolveAssetSource(require('@images/prawnImg.png')).uri,
-    isFavourite: true,
-    rating: 4.6,
-    cals: 130,
-    location: "Lagos, Nigeria"
+    name: "River prawn spicy soup",
+    description: "River prawn spiacy soup eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/prawnImg.png')).uri,
+    category: "Soups",
+    availability: "available",
+    pricing_method: "fixed",
+    price: 1500,
+    isFavourite: true
   },
   {
     id: "2",
     name: "Jollof Rice",
-    desc: "Jollof Rice eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
-    price: 2000,
-    imgUrl: Image.resolveAssetSource(require('@images/jollofRice.png')).uri,
-    isFavourite: true,
-    rating: 5,
-    cals: 898,
-    location: "Lagos, Nigeria"
+    description: "Jollof Rice eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/jollofRice.png')).uri,
+    category: "Carbohydrate",
+    availability: "available",
+    pricing_method: "portion",
+    price: 150,
+    isFavourite: true
   },
   {
     id: "3",
     name: "Macaroni",
-    desc: "Macaroni eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
-    price: 4300,
-    imgUrl: Image.resolveAssetSource(require('@images/macaroni.png')).uri,
-    isFavourite: true,
-    rating: 4.2,
-    cals: 340,
-    location: "Lagos, Nigeria"
+    description: "Macaroni eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/macaroni.png')).uri,
+    category: "Carbohydrate",
+    availability: "available",
+    pricing_method: "price",
+    price: 300,
+    isFavourite: true
   },
   {
     id: "4",
     name: "Chicken and Chips",
-    desc: "Chicken and Chips eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    description: "Chicken and Chips eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/chickenAndChips.png')).uri,
+    category: "Snacks",
+    availability: "available",
+    pricing_method: "fixed",
     price: 8700,
-    imgUrl: Image.resolveAssetSource(require('@images/chickenAndChips.png')).uri,
-    isFavourite: false,
-    rating: 3.9,
-    cals: 1730,
-    location: "Lagos, Nigeria"
+    isFavourite: false
   },
   {
     id: "5",
     name: "Rice and Stew",
-    desc: "Rice and Stew eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
-    price: 1200,
-    imgUrl: Image.resolveAssetSource(require('@images/riceAndStew.png')).uri,
-    isFavourite: true,
-    rating: 4.9,
-    cals: 138,
-    location: "Lagos, Nigeria"
+    description: "Rice and Stew eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/riceAndStew.png')).uri,
+    category: "Carbohydrate",
+    availability: "available",
+    pricing_method: "portion",
+    price: 150,
+    isFavourite: true
   },
   {
     id: "6",
     name: "Seafood Pasta",
-    desc: "Seafood Pasta eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
-    price: 10800,
-    imgUrl: Image.resolveAssetSource(require('@images/shrimpSpaghetti.png')).uri,
-    isFavourite: true,
-    rating: 4.1,
-    cals: 433,
-    location: "Lagos, Nigeria"
+    description: "Seafood Pasta eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/shrimpSpaghetti.png')).uri,
+    category: "Carbohydrate",
+    availability: "available",
+    pricing_method: "fixed",
+    price: 4800,
+    isFavourite: true
   },
   {
     id: "7",
     name: "Chicken and Chips",
-    desc: "Chicken and Chips eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    description: "Chicken and Chips eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/chickenAndChips.png')).uri,
+    category: "Snacks",
+    availability: "available",
+    pricing_method: "fixed",
     price: 4300,
-    imgUrl: Image.resolveAssetSource(require('@images/chickenAndChips.png')).uri,
-    isFavourite: false,
-    rating: 3.6,
-    cals: 1300,
-    location: "Lagos, Nigeria"
+    isFavourite: false
   },
   {
     id: "8",
     name: "River prawn spicy soap",
-    desc: "River prawn spiacy soup eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
-    price: 12340,
-    imgUrl: Image.resolveAssetSource(require('@images/prawnImg.png')).uri,
-    isFavourite: true,
-    rating: 3.8,
-    cals: 309,
-    location: "Lagos, Nigeria"
+    description: "River prawn spiacy soup eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/prawnImg.png')).uri,
+    category: "Soups",
+    availability: "available",
+    pricing_method: "package",
+    package_sizes: [
+      { name: "small", price: 1500 },
+      { name: "medium", price: 2500 },
+      { name: "large", price: 3500 },
+    ],
+    isFavourite: true
   },
   {
     id: "9",
     name: "Macaroni",
-    desc: "Macaroni eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    description: "Macaroni eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/macaroni.png')).uri,
+    category: "Carbohydrate",
+    availability: "available",
+    pricing_method: "fixed",
     price: 1700,
-    imgUrl: Image.resolveAssetSource(require('@images/macaroni.png')).uri,
-    isFavourite: false,
-    rating: 4.3,
-    cals: 310,
-    location: "Lagos, Nigeria"
+    isFavourite: false
   },
   {
     id: "10",
     name: "River prawn spicy soap",
-    desc: "River prawn spiacy soup eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    description: "River prawn spiacy soup eunde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.",
+    img_url: Image.resolveAssetSource(require('@images/prawnImg.png')).uri,
+    category: "Soups",
+    availability: "available",
+    pricing_method: "fixed",
     price: 3560,
-    imgUrl: Image.resolveAssetSource(require('@images/prawnImg.png')).uri,
-    isFavourite: true,
-    rating: 4.7,
-    cals: 240,
-    location: "Lagos, Nigeria"
+    isFavourite: true
   }
 ];
 
