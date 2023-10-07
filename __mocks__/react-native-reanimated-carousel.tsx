b485405aa64b81ffd47f936284884cb5292d2977
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-07 18:49:26 +0100

    HACO-47 Update some of the mock files

--- __mocks__/react-native-reanimated-carousel.tsx
+++ __mocks__/react-native-reanimated-carousel.tsx
@@ -4,13 +4,4 @@ import { View, ScrollView, Text } from 'react-native';
 const Carousel = React.forwardRef((props, ref: LegacyRef<View>) => <View ref={ref} {...props} />);
 
 export type ICarouselInstance = ScrollView;
-
-const Yin = () => {
-	const ref = useRef<ScrollView>(null);
-	return (
-		<ScrollView ref={ref}>
-			<Text>Hello</Text>
-		</ScrollView>
-	);
-};
 export default Carousel;
