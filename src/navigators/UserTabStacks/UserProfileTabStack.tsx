3e563a932f383229be71a026c97c847118c62934
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-12 15:25:39 +0100

    HACO-63 Add the new profile screens to the tab stack navigator

--- src/navigators/UserTabStacks/UserProfileTabStack.tsx
+++ src/navigators/UserTabStacks/UserProfileTabStack.tsx
@@ -1,7 +1,7 @@
 import { UserTabHeader } from "@components/navigation";
 import { UserProfileTabStackParamList } from "@interfaces";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { LocationScreen, PersonalDetailScreen, ProfileScreen } from "@screens/core";
+import { LocationScreen, PersonalDetailScreen, ProfileScreen, VerifyEmailScreen, VerifyPhoneScreen } from "@screens/core";
 import React from "react";
 
 const Stack = createNativeStackNavigator<UserProfileTabStackParamList>();
@@ -12,6 +12,8 @@ const UserProfileTabStack = () => {
       <Stack.Screen name="Main" component={ProfileScreen} options={{ header: () => <UserTabHeader /> }} />
       <Stack.Screen name="UserDetails" component={PersonalDetailScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Location" component={LocationScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="VerifyPhone" component={VerifyPhoneScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="VerifyEmail" component={VerifyEmailScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
