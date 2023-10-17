f19b414821835d4866b641d10d4080d2af36499f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-17 02:35:36 +0100

    HACO-69 Refactor the location element component to also accept a initialmode and idx prop used to calculate the entering animation with reanimated 3

--- src/components/core/Profile/LocationElement.tsx
+++ src/components/core/Profile/LocationElement.tsx
@@ -1,24 +1,32 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react';
-import { useAppTheme } from '@hooks';
 import { Feather } from '@expo/vector-icons';
-import { fonts } from '@utils';
+import { useAppTheme } from '@hooks';
 import { LocationInterface } from '@interfaces';
+import { fonts } from '@utils';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Animated, { FadeInUp, SlideOutLeft } from 'react-native-reanimated';
 
 interface LocationElementInterface {
   location: LocationInterface;
+  initialMode: React.MutableRefObject<boolean>;
+  idx: number;
   removeLocation: (id: string) => void;
 }
 
-const LocationElement = ({ location: { name, id }, removeLocation }: LocationElementInterface) => {
+const LocationElement = ({ location: { name, locationId }, initialMode, idx, removeLocation }: LocationElementInterface) => {
   const { color } = useAppTheme();
   return (
-    <View style={[styles.container, { borderColor: color.mainGreen }]} testID='location element'>
+    <Animated.View
+      entering={FadeInUp.delay(initialMode.current ? 100 * idx + 100 : 0)}
+      exiting={SlideOutLeft.duration(100)}
+      style={[styles.container, { borderColor: color.mainGreen }]}
+      testID="location element"
+    >
       <Text style={[styles.location, { color: color.mainText }]}>{name}</Text>
-      <TouchableOpacity onPress={() => removeLocation(id)} testID='remove button'>
+      <TouchableOpacity onPress={() => removeLocation(locationId)} testID={`remove button ${locationId}`}>
         <Feather name="trash-2" size={24} color="rgba(233, 50, 35, .6)" />
       </TouchableOpacity>
-    </View>
+    </Animated.View>
   );
 };
 
