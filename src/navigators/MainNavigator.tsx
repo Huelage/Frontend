fa1ff66109b4ff0f7af440d9a9dd8251b769f3ab
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-18 18:49:49 +0100

    HACO-64 Add the expo-status-bar component to the main navigator which effectively eliminates the need to add it to every component

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -7,6 +7,7 @@ import { useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AuthStackNavigator from "./AuthStackNavigator";
 import StackNavigator from "./StackNavigator";
+import { StatusBar } from "expo-status-bar";
 
 const MainNavigator = () => {
   const dispatch = useAppDispatch();
@@ -19,6 +20,7 @@ const MainNavigator = () => {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
       <NavigationContainer theme={theme === "dark" ? DarkTheme : DefaultTheme}>
         {!!isSignedIn ? <StackNavigator /> : <AuthStackNavigator />}
       </NavigationContainer>
