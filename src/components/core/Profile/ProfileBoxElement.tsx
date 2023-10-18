11e9ab71b4874d961595a8e0a50ecabdc9492ec9
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-18 18:46:30 +0100

    HACO-64 Add a component that renders the ui for each profile screen navigation element

---
+++ src/components/core/Profile/ProfileBoxElement.tsx
@@ -0,0 +1,52 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useAppTheme } from '@hooks';
+import { UserProfileTabProps, UserProfileTabStackParamList } from '@interfaces';
+import { useNavigation } from '@react-navigation/native';
+import { fonts } from '@utils';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export interface ElementInterface {
+  label: string;
+  nav: keyof UserProfileTabStackParamList;
+  icon: keyof typeof MaterialCommunityIcons.glyphMap;
+}
+
+const ProfileBoxElement = ({ label, icon, nav }: ElementInterface) => {
+  const { color } = useAppTheme();
+  const { navigate } = useNavigation<UserProfileTabProps>();
+  return (
+    <TouchableOpacity onPress={() => navigate(nav)} testID='profile box element'>
+      <View style={styles.container}>
+        <MaterialCommunityIcons name={icon} size={35} color={color.mainGreen} testID='element icon' />
+        <View style={styles.detailBox}>
+          <Text style={[styles.textStyle, { color: color.mainText }]}>{label}</Text>
+          <MaterialCommunityIcons name="chevron-right" size={30} color={color.mainText} />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ProfileBoxElement;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: "row",
+    gap: 20,
+    justifyContent: "center",
+    marginHorizontal: 20,
+    paddingVertical: 20
+  },
+  detailBox: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textStyle: {
+    fontFamily: fonts.I_500,
+    fontSize: 20
+  },
+});
--- src/components/core/Profile/index.ts
+++ src/components/core/Profile/index.ts
@@ -1,6 +1,6 @@
-export { default as ProfileInfo } from "./ProfileInfo";
-export { default as ProfileList } from "./ProfileList";
-export { default as ProfileList2 } from "./ProfileList2";
 export { default as DetailElement } from "./DetailElement";
-export { default as LocationInput } from "./LocationInput";
 export { default as LocationElement } from "./LocationElement";
+export { default as LocationInput } from "./LocationInput";
+export { default as ProfileBoxElement, ElementInterface } from './ProfileBoxElement';
+export { default as ProfileNavBox } from './ProfileNavBox';
+
