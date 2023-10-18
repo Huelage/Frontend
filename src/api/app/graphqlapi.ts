f87a26ee1bd710094e083741a8baebfbdbbbacae
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-18 18:52:47 +0100

    HACO-64 Refactor the graphql client to handle invalid accessToken scenerios

--- src/api/app/graphqlapi.ts
+++ src/api/app/graphqlapi.ts
@@ -1,68 +1,76 @@
 import { store } from '@api/app/store';
 import { REFRESH_ACCESS_TOKEN } from '@api/graphql';
-import { setCredentials } from '@api/slices/globalSlice';
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, fromPromise } from '@apollo/client';
+import { clearCredentials, setCredentials } from '@api/slices/globalSlice';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, Operation, fromPromise } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
+import { RetryLink } from "@apollo/client/link/retry";
 import { getItem, showError } from '@utils';
 
+const isRefreshing = (operation: Operation) => {
+  return operation.operationName === 'RefreshAccessToken';
+};
+const getToken = async (operation: Operation) => {
+  if (isRefreshing(operation)) {
+    const refreshToken = await getItem("huelageRefreshToken");
+    return refreshToken;
+  } else {
+    return store.getState().global.accessToken;
+  }
+};
 const getNewAccessToken = async () => {
   try {
-    console.log('Refreshing access token');
-    const { data: { refreshAccessToken } } = await graphqlclient.mutate({ mutation: REFRESH_ACCESS_TOKEN, fetchPolicy: 'network-only' });
-    console.log({ refreshAccessToken });
-    store.dispatch(setCredentials({ accessToken: refreshAccessToken }));
-    return refreshAccessToken;
+    const { data: { refreshAccessToken: accessToken } } = await graphqlclient.mutate({ mutation: REFRESH_ACCESS_TOKEN });
+    store.dispatch(setCredentials({ accessToken: accessToken as string }));
+    return accessToken;
   } catch (err) {
-    console.log("There was an error refreshing the access token");
-    console.log(err);
+    store.dispatch(clearCredentials());
+    return Promise.reject(err);
   }
 };
 
 const httpLink = new HttpLink({ uri: process.env.EXPO_PUBLIC_GRAPHQL_URL });
 const authMiddleware = new ApolloLink((operation, forward) => {
-  const token = store.getState().global.accessToken;
-  operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : ''
-    }
+  return fromPromise(getToken(operation)).flatMap(token => {
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+    return forward(operation);
   });
-  return forward(operation);
 });
 const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
   if (graphQLErrors) {
-    graphQLErrors.map(async ({ message }) => {
+    graphQLErrors.map(({ message }) => {
       if (message === 'Unauthorized') {
-        console.log(`[GraphQL Error]: ${message}`);
-        const refreshToken = await getItem("huelageRefreshToken");
-        console.log({ refreshToken });
-        const oldHeaders = operation.getContext().headers;
-        operation.setContext({
-          headers: {
-            authorization: `Bearer ${refreshToken}`
-          }
-        });
-        await getNewAccessToken();
-        const token = store.getState().global.accessToken;
-        console.log({ token });
-        operation.setContext({
-          headers: {
-            Authorization: token ? `Bearer ${token}` : ''
-          }
-        });
-        console.log(`[GraphQL Error]: ${message}`);
-        return forward(operation);
+        if (isRefreshing(operation)) return forward(operation);
+        return fromPromise(getNewAccessToken().then(token => {
+          operation.setContext(({ headers = {} }) => ({
+            headers: {
+              ...headers,
+              authorization: `Bearer ${token}`
+            }
+          }));
+        })).flatMap(() => forward(operation));
       }
-      else {
+      else
         showError(message);
-      }
     });
   }
   if (networkError) {
     console.log(`[Network Error]: ${networkError}`);
   }
 });
+const retryLink = new RetryLink({
+  attempts: (count, operation, error) => {
+    return error.message === 'Unauthorized';
+  },
+  delay: (count, operation, error) => {
+    return count * 1000;
+  }
+});
 const graphqlclient = new ApolloClient({
-  link: ApolloLink.from([errorLink, authMiddleware, httpLink]),
+  link: ApolloLink.from([authMiddleware, errorLink, retryLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
