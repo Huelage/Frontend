5b2bc0ddd9f2d1ccb6e2ea65cbef2423b7b2b857
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-21 01:21:27 +0100

    HACO-64 Add a component that displays a setting group

---
+++ src/components/core/Profile/SettingElement.tsx
@@ -0,0 +1,59 @@
+import { useAppTheme } from '@hooks';
+import { SettingElementInterface } from '@interfaces';
+import { fonts } from '@utils';
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import SettingOptionBox from './SettingOptionBox';
+
+
+const SettingElement = ({ title, Icon, options }: SettingElementInterface) => {
+  const { color } = useAppTheme();
+  return (
+    <View style={styles.container} testID='setting element'>
+      <View style={[styles.iconBox, { backgroundColor: color.mainGreen }]} testID='setting icon'>
+        <Icon />
+      </View>
+      <View style={styles.detailBox}>
+        <Text style={[styles.title, { color: color.mainText }]}>{title}</Text>
+        <FlatList
+          style={styles.contentContainer}
+          data={options}
+          scrollEnabled={false}
+          keyExtractor={(_, idx) => idx.toString()}
+          renderItem={({ item }) => <SettingOptionBox {...item} />}
+          testID='setting box list'
+        />
+      </View>
+    </View>
+  );
+};
+
+export default React.memo(SettingElement);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    gap: 15,
+    padding: 20
+  },
+  contentContainer: {
+    gap: 10
+  },
+  iconBox: {
+    alignItems: 'center',
+    borderRadius: 20,
+    height: 40,
+    justifyContent: 'center',
+    width: 40
+  },
+  detailBox: {
+    flex: 1,
+    gap: 15,
+    paddingTop: 8
+  },
+  title: {
+    fontFamily: fonts.I_600,
+    fontSize: 22,
+    letterSpacing: 1
+  }
+});
