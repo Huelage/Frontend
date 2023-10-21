bc964cd840fb79ad66389d1019667df4b9a843c6
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-21 01:36:06 +0100

    HACO-64 Update the stack navigator such that the showOnboard state is updated so the onboard screen can automatically navigate to the login screen if the logout button is clicked

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -1,11 +1,17 @@
-import { useAppSelector } from "@api/app/appHooks";
-import { getVendorStatus } from "@api/slices/globalSlice";
-import React from "react";
+import { useAppDispatch, useAppSelector } from "@api/app/appHooks";
+import { getShowOnboard, getVendorStatus, setShowOnboard } from "@api/slices/globalSlice";
+import React, { useEffect } from "react";
 import UserStackNavigator from "./UserStackNavigator";
 import VendorStackNavigator from "./VendorStackNavigator";
 
 const StackNavigator = () => {
+  const dispatch = useAppDispatch();
   const isVendor = useAppSelector(getVendorStatus);
+  const showOnboard = useAppSelector(getShowOnboard);
+  useEffect(() => {
+    if (showOnboard)
+      dispatch(setShowOnboard(false));
+  }, []);
   return <>{isVendor ? <VendorStackNavigator /> : <UserStackNavigator />}</>;
 };
 
