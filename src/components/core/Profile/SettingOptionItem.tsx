a33b414ce8b6ff6df3dcc98f1c2da007a242c9e4
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-21 01:25:25 +0100

    HACO-64 Add a component that renders each individual setting item be it a toggle or a nav box

---
+++ src/components/core/Profile/SettingOptionItem.tsx
@@ -0,0 +1,56 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useAppTheme } from '@hooks';
+import { SettingOptionInterface } from '@interfaces';
+import { fonts } from '@utils';
+import React, { useState } from 'react';
+import { StyleSheet, Switch, Text, TouchableOpacity, View } from 'react-native';
+
+const SettingOptionItem = ({ title, isToggle, initVal, disabled, danger, onPress }: SettingOptionInterface) => {
+  const { color } = useAppTheme();
+  const [isToggleEnabled, setIsToggleEnabled] = useState<boolean | undefined>(initVal);
+  const toggleSwitch = () => {
+    setIsToggleEnabled(!isToggleEnabled);
+    onPress();
+  };
+  return (
+    <TouchableOpacity disabled={isToggle} onPress={onPress} testID={`${title} setting option item`}>
+      <View style={styles.container}>
+        <Text style={[styles.textStyle, { color: danger ? "rgba(233, 50, 35, .6)" : color.mainText }]}>{title}</Text>
+        {isToggle ? (
+          <Switch
+            disabled={disabled}
+            trackColor={{ false: 'rgba(188, 181, 181, .3)', true: color.mainGreenOpaque }}
+            thumbColor={isToggleEnabled ? color.mainGreen : "rgb(188, 181, 181)"}
+            ios_backgroundColor="rgba(188, 181, 181, .3)"
+            onValueChange={toggleSwitch}
+            value={isToggleEnabled}
+            style={styles.switchStyle}
+            testID={`${title} toggle switch`}
+          />
+        ) : (
+          <MaterialCommunityIcons testID='chevron icon' name="chevron-right" size={30} color={danger ? "rgba(233, 50, 35, .6)" : color.mainText} />
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default React.memo(SettingOptionItem);
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: "row",
+    gap: 20,
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    paddingVertical: 10
+  },
+  textStyle: {
+    fontFamily: fonts.I_400,
+    fontSize: 18
+  },
+  switchStyle: {
+    transform: [{ scale: .8 }]
+  }
+});
--- src/components/core/Profile/index.ts
+++ src/components/core/Profile/index.ts
@@ -1,9 +1,17 @@
-export { default as DetailElement } from "./DetailElement";
+// About
+// FAQ
+// Help
+// Location
 export { default as LocationElement } from "./LocationElement";
 export { default as LocationInput } from "./LocationInput";
-export { default as ProfileBoxElement, ElementInterface } from './ProfileBoxElement';
+// Personal Detail
+export { default as DetailElement } from "./DetailElement";
+// Profile
+export { default as ProfileBoxElement } from './ProfileBoxElement';
 export { default as ProfileNavBox } from './ProfileNavBox';
-
-export { default as GeneralSetting } from "./GeneralSetting";
-export { default as NotificationSetting } from "./NotificationSettting";
-export { default as PrivacySetting } from "./PrivacySetting";
+// Referral
+// Setting
+export { default as SettingElement } from './SettingElement';
+export { default as SettingOptionBox } from './SettingOptionBox';
+export { default as SettingOptionItem } from './SettingOptionItem';
+// Wallet
