632fc1056728b1e156341533c37c28ed633b3e95
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-21 01:34:22 +0100

    HACO-64 Refactor the main navigator such that the device's color scheme is only used if the themeType is 'system'

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -1,23 +1,25 @@
 import { useAppDispatch, useAppSelector } from "@api/app/appHooks";
-import { getEntity, switchTheme } from "@api/slices/globalSlice";
+import { getAccessToken, getThemeType, switchTheme } from "@api/slices/globalSlice";
 import { useAppTheme } from "@hooks";
 import { DarkTheme, DefaultTheme, NavigationContainer, } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AuthStackNavigator from "./AuthStackNavigator";
 import StackNavigator from "./StackNavigator";
-import { StatusBar } from "expo-status-bar";
 
 const MainNavigator = () => {
   const dispatch = useAppDispatch();
-  const isSignedIn = useAppSelector(getEntity);
+  const isSignedIn = useAppSelector(getAccessToken);
+  const themeType = useAppSelector(getThemeType);
   const { theme } = useAppTheme();
   const nativeTheme = useColorScheme();
-  useEffect(() => {
-    dispatch(switchTheme(nativeTheme || "light"));
-  }, [nativeTheme]);
 
+  useEffect(() => {
+    if (themeType === 'system')
+      dispatch(switchTheme(nativeTheme || "dark"));
+  }, [nativeTheme]);
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
