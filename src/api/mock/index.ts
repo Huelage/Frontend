4f3cece8f3a81749a638c3b754c2216cdf465dc0
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-25 15:04:53 +0100

    HACO-76 Add a file containing the mock orderItems

--- src/api/mock/index.ts
+++ src/api/mock/index.ts
@@ -1,5 +1,6 @@
+export { default as mockCartItems } from "./mockCartItem";
 export { default as mockCategories } from "./mockCategoryData";
 export { default as mockFoods } from "./mockFoodData";
+export { default as mockOrderItems } from './mockOrderItems';
 export { default as mockRestaurants } from "./mockRestaurants";
-export { default as mockCartItems } from "./mockCartItem";
 export { default as mockReviews } from "./mockVendorReview";
