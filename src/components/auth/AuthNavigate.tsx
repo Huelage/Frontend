effd4faa28d4034323630266c88b744fcb530b28
Toby2507 <oluwatobisalau2000@gmail.com>
2023-11-04 20:58:17 +0100

    HACO-96 Navigate to the signup select  from the login screen instead of the signup screen directly

--- src/components/auth/AuthNavigate.tsx
+++ src/components/auth/AuthNavigate.tsx
@@ -1,31 +1,31 @@
-import { useAppTheme } from '@hooks';
-import { AuthNavigationProps } from '@interfaces';
-import { useNavigation } from '@react-navigation/native';
-import { fonts } from '@utils';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useAppTheme } from "@hooks";
+import { AuthNavigationProps } from "@interfaces";
+import { useNavigation } from "@react-navigation/native";
+import { fonts } from "@utils";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 interface AuthNavigateProps {
-  page: 'SU' | 'SI',
+  page: "SU" | "SI",
 }
 
 const AuthNavigate = ({ page }: AuthNavigateProps) => {
   const { color } = useAppTheme();
   const { navigate } = useNavigation<AuthNavigationProps>();
   const handleNavigate = () => {
-    if (page == 'SU') {
-      navigate('Login');
+    if (page == "SU") {
+      navigate("Login");
     } else {
-      navigate('SignUp');
+      navigate("SignupSelect");
     }
   };
   return (
-    <View style={styles.accountBox} testID='auth navigate'>
+    <View style={styles.accountBox} testID="auth navigate">
       <Text style={[styles.accountRedirect, { color: color.mainText }]}>
-        {page == 'SU' ? "Already a member" : "Don't have an account"}?
+        {page == "SU" ? "Already a member" : "Don't have an account"}?
       </Text>
       <TouchableOpacity accessibilityRole="button" onPress={handleNavigate}>
-        <Text style={[styles.accountAccent, { color: color.mainGreen }]}>{page == 'SU' ? "Login" : "Sign Up"}</Text>
+        <Text style={[styles.accountAccent, { color: color.mainGreen }]}>{page == "SU" ? "Login" : "Sign Up"}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -35,15 +35,15 @@ export default AuthNavigate;
 
 const styles = StyleSheet.create({
   accountBox: {
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
     gap: 5,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
   accountRedirect: {
     fontFamily: fonts.I_400,
     fontSize: 15,
-    textAlign: 'center'
+    textAlign: "center"
   },
   accountAccent: {
     fontFamily: fonts.I_700,
