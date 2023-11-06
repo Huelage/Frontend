6aaadba207f00df3107e6dbf7a11e2f16f9caffe
Toby2507 <oluwatobisalau2000@gmail.com>
2023-11-06 12:31:04 +0100

    HACO-79 Update the screen navigators to vendors tab stack

--- src/navigators/UserTabStacks/UserVendorsTabStack.tsx
+++ src/navigators/UserTabStacks/UserVendorsTabStack.tsx
@@ -1,7 +1,7 @@
 import { UserTabHeader } from "@components/navigation";
 import { UserVendorsTabStackParamList } from "@interfaces";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ItemDetailScreen, VendorScreen } from "@screens/core";
+import { ItemDetailScreen, VendorListScreen, VendorScreen } from "@screens/core";
 import React from "react";
 
 const Stack = createNativeStackNavigator<UserVendorsTabStackParamList>();
@@ -9,7 +9,8 @@ const Stack = createNativeStackNavigator<UserVendorsTabStackParamList>();
 const UserVendorsTabStack = () => {
   return (
     <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen name="Main" component={VendorScreen} options={{ header: () => <UserTabHeader /> }} />
+      <Stack.Screen name="Main" component={VendorListScreen} options={{ header: () => <UserTabHeader /> }} />
+      <Stack.Screen name="VendorHome" component={VendorScreen} options={{ headerShown: false }} />
       <Stack.Screen name="ItemDetail" component={ItemDetailScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
