f7e0ff733789fdaef1c0f73500166861af331d8f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-11-06 12:27:03 +0100

    HACO-79 Add a component that renders the vendor product for the vendor home page

--- src/components/core/Vendor/index.ts
+++ src/components/core/Vendor/index.ts
@@ -1,2 +1,3 @@
 export { default as SideOptionElement } from "./SideOptionElement";
+export { default as VendorProduct } from "./VendorProduct";
 export { default as VendorResCard } from "./VendorResCard";
