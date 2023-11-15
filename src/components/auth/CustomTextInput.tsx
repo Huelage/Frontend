9e7bb3671573cf2b8add795e53cab5da4f2ba2eb
Toby2507 <oluwatobisalau2000@gmail.com>
2023-11-15 18:34:16 +0100

    HACO-99 Add a new optional prop isDrop to render a dropdown list to the customTextInput

--- src/components/auth/CustomTextInput.tsx
+++ src/components/auth/CustomTextInput.tsx
@@ -1,3 +1,4 @@
+import { CustomDropdown } from "@components/misc";
 import { Ionicons } from "@expo/vector-icons";
 import { useAppTheme } from "@hooks";
 import { fonts } from "@utils";
@@ -8,13 +9,15 @@ import PhoneInput from "react-native-phone-input";
 
 interface CustomTextInputProps extends TextInputProps {
   label: string;
-  isPhone?: boolean;
-  isPass?: boolean;
+  data?: { key: string, value: string, disabled?: boolean; }[];
   error?: FieldError;
   innerRef?: RefCallBack;
+  isDrop?: boolean;
+  isPass?: boolean;
+  isPhone?: boolean;
 }
 
-const CustomTextInput = ({ label, isPhone, isPass, error, ...inputProps }: CustomTextInputProps) => {
+const CustomTextInput = ({ data, label, isPhone, isDrop, isPass, error, ...inputProps }: CustomTextInputProps) => {
   const [showText, setShowText] = useState<boolean>(!isPass);
   const { color, theme } = useAppTheme();
 
@@ -24,52 +27,56 @@ const CustomTextInput = ({ label, isPhone, isPass, error, ...inputProps }: Custo
       {isPhone ? (
         <Text style={[styles.placeholder, { color: color.mainTextDim }]}>{label}</Text>
       ) : null}
-      <View style={[styles.inputContainer, error && styles.inputContainerError]}>
-        <View style={styles.input}>
-          {isPhone ? (
-            <PhoneInput
-              autoFormat
-              initialCountry="ng"
-              onChangePhoneNumber={inputProps.onChangeText}
-              ref={inputProps.innerRef || undefined}
-              textProps={{
-                style: [styles.textInput, { color: color.mainText }],
-                placeholder: label, placeholderTextColor: color.mainTextDim,
-                selectionColor: error ? color.danger : color.mainGreen,
-                secureTextEntry: !showText,
-                autoCapitalize: inputProps.autoCapitalize || "none",
-                autoCorrect: inputProps.autoCorrect || false,
-                keyboardType: inputProps.keyboardType || "number-pad",
-                onSubmitEditing: inputProps.onSubmitEditing,
-                returnKeyType: inputProps.returnKeyType || "next",
-                testID: "phone input"
-              }}
-            />
-          ) : (
-            <TextInput
-              blurOnSubmit={false}
-              secureTextEntry={!showText}
-              style={[styles.textInput, { color: color.mainText }]}
-              placeholder={label}
-              placeholderTextColor={color.mainTextDim}
-              selectionColor={error ? color.danger : color.mainGreen}
-              ref={inputProps.innerRef || undefined}
-              {...inputProps}
-            />
-          )}
-          {isPass && (
-            showText ? (
-              <TouchableOpacity testID="pass-visibility toggle" onPress={toggleShowText}>
-                <Ionicons size={24} color={color.mainText} name="eye-off" style={{ opacity: .7 }} />
-              </TouchableOpacity>
+      {isDrop ? (
+        <CustomDropdown data={data ?? []} isError={!!error} label={label} onChange={inputProps.onChangeText!} />
+      ) : (
+        <View style={[styles.inputContainer, { borderColor: error ? color.danger : color.mainGreen }]}>
+          <View style={styles.input}>
+            {isPhone ? (
+              <PhoneInput
+                autoFormat
+                initialCountry="ng"
+                onChangePhoneNumber={inputProps.onChangeText}
+                ref={inputProps.innerRef || undefined}
+                textProps={{
+                  style: [styles.textInput, { color: color.mainText }],
+                  placeholder: label, placeholderTextColor: color.mainTextDim,
+                  selectionColor: error ? color.danger : color.mainGreen,
+                  secureTextEntry: !showText,
+                  autoCapitalize: inputProps.autoCapitalize || "none",
+                  autoCorrect: inputProps.autoCorrect || false,
+                  keyboardType: inputProps.keyboardType || "number-pad",
+                  onSubmitEditing: inputProps.onSubmitEditing,
+                  returnKeyType: inputProps.returnKeyType || "next",
+                  testID: "phone input"
+                }}
+              />
             ) : (
-              <TouchableOpacity testID="pass-visibility toggle" onPress={toggleShowText}>
-                <Ionicons size={24} color={color.mainText} name="eye" style={{ opacity: .7 }} />
-              </TouchableOpacity>
-            )
-          )}
+              <TextInput
+                blurOnSubmit={false}
+                secureTextEntry={!showText}
+                style={[styles.textInput, { color: color.mainText }]}
+                placeholder={label}
+                placeholderTextColor={color.mainTextDim}
+                selectionColor={error ? color.danger : color.mainGreen}
+                ref={inputProps.innerRef || undefined}
+                {...inputProps}
+              />
+            )}
+            {isPass && (
+              showText ? (
+                <TouchableOpacity testID="pass-visibility toggle" onPress={toggleShowText}>
+                  <Ionicons size={24} color={color.mainText} name="eye-off" style={{ opacity: .7 }} />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity testID="pass-visibility toggle" onPress={toggleShowText}>
+                  <Ionicons size={24} color={color.mainText} name="eye" style={{ opacity: .7 }} />
+                </TouchableOpacity>
+              )
+            )}
+          </View>
         </View>
-      </View>
+      )}
       {error && <View style={[styles.errorBox, { backgroundColor: theme === "dark" ? "#181818" : "#000" }]}>
         <Text style={styles.errorText}>
           <Ionicons name="alert-circle-outline" size={16} color="white" />&nbsp;
@@ -87,7 +94,6 @@ const styles = StyleSheet.create({
     gap: 3
   },
   inputContainer: {
-    borderColor: "#47CA4C",
     borderRadius: 10,
     borderWidth: 2,
     height: 40,
