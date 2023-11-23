3b74b92dc0ce052c46c2f054147cc7093bc2687f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-11-23 12:43:25 +0100

    HACO-102 Add the query to get vendors product

--- src/api/graphql/vendor.queries.ts
+++ src/api/graphql/vendor.queries.ts
@@ -1,5 +1,25 @@
 import { gql } from "@apollo/client";
 
+export const GET_PRODUCTS = gql(`
+  query GetProducts ($vendorId:String!) {
+    getVendorProducts(vendorId:$vendorId) {
+      productId
+      name
+      description
+      imgUrl
+      food {
+        category
+        pricingMethod
+        price
+        preparationTime
+        availability
+        packageSizes
+        sides
+      }
+    }
+  }
+`);
+
 export const ADD_FOOD_ITEM = gql(`
   mutation AddFoodItem ($input: CreateFoodInput!) {
     addFood(input: $input) {
