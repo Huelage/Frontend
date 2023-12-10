1411c36564674911f2cb01ead9c45def02c8de4f
Toby2507 <oluwatobisalau2000@gmail.com>
2023-12-10 06:24:38 +0100

    HACO-1-4 Add the screen navigator for the OrderDetail screen in the vendor order tab stack

--- src/navigators/VendorTabStacks/VendorOrdersTabStack.tsx
+++ src/navigators/VendorTabStacks/VendorOrdersTabStack.tsx
@@ -1,15 +1,16 @@
 import { VendorTabHeader } from "@components/navigation";
 import { VendorOrdersTabStackParamList } from "@interfaces";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { OrderScreen } from "@screens/Vendor";
+import { OrderDetailScreen, OrderScreen } from "@screens/Vendor";
 import React from "react";
 
 const Stack = createNativeStackNavigator<VendorOrdersTabStackParamList>();
 
 const VendorOrdersTabStack = () => {
   return (
-    <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen name="Main" component={OrderScreen} options={{ header: () => <VendorTabHeader /> }} />
+    <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Main" component={OrderScreen} options={{ header: () => <VendorTabHeader />, headerShown: true }} />
+      <Stack.Screen name="OrderDetail" component={OrderDetailScreen} />
     </Stack.Navigator>
   );
 };
