37b5d3536d8a37073f3f53d18edb184feaaaf939
Toby2507 <oluwatobisalau2000@gmail.com>
2023-12-14 00:07:39 +0100

    HACO-106 Move some of the core profile screens into a general folder since they would be reused in the vendor flow as well

---
+++ src/screens/General/index.ts
@@ -0,0 +1,7 @@
+export { default as AboutScreen } from "./AboutScreen";
+export { default as ChangePasswordScreen } from "./ChangePasswordScreen";
+export { default as ChangePhoneScreen } from "./ChangePhoneScreen";
+export { default as FAQScreen } from "./FAQScreen";
+export { default as HelpScreen } from "./HelpScreen";
+export { default as VerifyEmailScreen } from "./VerifyEmailScreen";
+export { default as VerifyPhoneScreen } from "./VerifyPhoneScreen";
