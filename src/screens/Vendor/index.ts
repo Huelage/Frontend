7539f718994c3f1c4c60e2491b48b931df07b12d
Toby2507 <oluwatobisalau2000@gmail.com>
2023-12-14 00:14:07 +0100

    HACO-106 Cleanup the screen index.ts files

--- src/screens/Vendor/index.ts
+++ src/screens/Vendor/index.ts
@@ -1,9 +1,13 @@
-export { default as AccountScreen } from "./AccountScreen";
 export { default as HomeScreen } from "./HomeScreen";
-export { default as NotificationScreen } from "./NotificationScreen";
+// Account
+export { default as AccountDetailScreen } from "./Account/AccountDetailScreen";
+export { default as AccountScreen } from "./Account/AccountScreen";
+export { default as SettingScreen } from "./Account/SettingScreen";
+export { default as WalletScreen } from "./Account/WalletScreen";
 // Menu
 export { default as AddItemScreen } from "./Menu/AddItemScreen";
 export { default as MenuScreen } from "./Menu/MenuScreen";
 // Order
 export { default as OrderDetailScreen } from "./Order/OrderDetailScreen";
 export { default as OrderScreen } from "./Order/OrderScreen";
+
--- src/screens/core/index.ts
+++ src/screens/core/index.ts
@@ -4,18 +4,11 @@ export { default as HomeScreen } from "./HomeScreen";
 export { default as OrderDetailScreen } from "./Orders/OrderDetailScreen";
 export { default as OrderScreen } from "./Orders/OrderScreen";
 // Profile Screens
-export { default as AboutScreen } from "./Profile/AboutScreen";
-export { default as ChangePasswordScreen } from "./Profile/ChangePasswordScreen";
-export { default as ChangePhoneScreen } from "./Profile/ChangePhoneScreen";
-export { default as FAQScreen } from "./Profile/FAQScreen";
-export { default as HelpScreen } from "./Profile/HelpScreen";
 export { default as LocationScreen } from "./Profile/LocationScreen";
 export { default as PersonalDetailScreen } from "./Profile/PersonalDetailScreen";
 export { default as ProfileScreen } from "./Profile/ProfileScreen";
 export { default as ReferralScreen } from "./Profile/ReferralScreen";
 export { default as SettingScreen } from "./Profile/SettingScreen";
-export { default as VerifyEmailScreen } from "./Profile/VerifyEmailScreen";
-export { default as VerifyPhoneScreen } from "./Profile/VerifyPhoneScreen";
 export { default as WalletScreen } from "./Profile/WalletScreen";
 // Vendor Screens
 export { default as ItemDetailScreen } from "./Vendors/ItemDetailScreen";
