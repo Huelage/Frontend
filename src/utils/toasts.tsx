3d1111db5f806310ab45ab735cf64e0f60404c4e
Toby2507 <oluwatobisalau2000@gmail.com>
2023-12-17 09:06:35 +0100

    HACO-107 REfactor the toast functions to use the user selected toast type between none, banner and popup

--- src/utils/toasts.tsx
+++ src/utils/toasts.tsx
@@ -2,29 +2,51 @@ import { store } from "@api/app/store";
 import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
 import { showMessage } from "react-native-flash-message";
+import Toast from "react-native-root-toast";
 
 export const showError = (message: string) => {
-  const allowToast = store.getState().global.allowToast;
-  if (!allowToast) return;
-  showMessage({
-    message,
-    type: "danger",
-    duration: 3000,
-    icon: () => <MaterialIcons name="error" size={24} color="black" />,
-    style: styles.toast
-  });
+  const toastType = store.getState().global.toastType;
+  if (toastType === "banner") {
+    showMessage({
+      message,
+      type: "danger",
+      duration: 3000,
+      icon: () => <MaterialIcons name="error" size={24} color="black" />,
+      style: styles.toast
+    });
+  } else {
+    Toast.show(message, {
+      duration: 3000,
+      backgroundColor: "#d9534f",
+      opacity: 1,
+      textColor: "white",
+      position: Toast.positions.TOP,
+      shadowColor: "rgba(233, 53, 37, .15)"
+    })
+  }
 };
 
 export const showSuccess = (message: string) => {
-  const allowToast = store.getState().global.allowToast;
-  if (!allowToast) return;
-  showMessage({
-    message,
-    type: "success",
-    duration: 3000,
-    icon: () => <AntDesign name="checkcircleo" size={24} color="white" />,
-    style: styles.toast
-  });
+  const toastType = store.getState().global.toastType;
+  if (toastType === "none") return;
+  if (toastType === "banner") {
+    showMessage({
+      message,
+      type: "success",
+      duration: 3000,
+      icon: () => <AntDesign name="checkcircleo" size={24} color="white" />,
+      style: styles.toast
+    });
+  } else {
+    Toast.show(message, {
+      duration: 3000,
+      backgroundColor: "#5cb85c",
+      opacity: 1,
+      textColor: "white",
+      position: Toast.positions.TOP,
+      shadowColor: "rgba(91, 207, 95, .3)"
+    })
+  }
 };
 
 const styles = StyleSheet.create({
