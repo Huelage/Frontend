cc924130e2e54728da0e82c7c6108ef40da84873
Toby2507 <oluwatobisalau2000@gmail.com>
2023-12-17 10:14:46 +0100

    HACO-105 Use the onLayout to compute the height of the order detail item background box

--- src/containers/User/Order/OrderDetailItem.tsx
+++ src/containers/User/Order/OrderDetailItem.tsx
@@ -2,19 +2,27 @@ import { CustomBox } from "@components/misc";
 import { useAppTheme } from "@hooks";
 import { OrderItemInterface } from "@interfaces";
 import { fonts, numberToCurrency } from "@utils";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState } from "react";
+import { LayoutChangeEvent, StyleSheet, Text, View } from "react-native";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const OrderDetailItem = ({ quantity, extras, totalPrice, item_name }: OrderItemInterface) => {
   const { color } = useAppTheme();
+  const [height, setHeight] = useState<number>(0);
+
+  const handleOnLayout = useCallback((e: LayoutChangeEvent) => {
+    const { height } = e.nativeEvent.layout;
+    setHeight(height);
+  }, []);
   return (
-    <View style={styles.container} testID="order detail item">
-      <CustomBox width={wp("100%") - 30} height={100} r={20} pad={6} left={-4} />
+    <View onLayout={handleOnLayout} style={styles.container} testID="order detail item">
+      <CustomBox width={wp("100%") - 30} height={height + 10} r={20} pad={6} left={-4} />
       <Text style={[styles.itemQuantity, { color: color.mainText }]} testID="order item quantity">x{quantity}</Text>
       <View style={styles.detailBox}>
         <Text style={[styles.itemName, { color: color.mainText }]} numberOfLines={1} testID="order item name">{item_name}</Text>
-        <Text style={[styles.itemExtras, { color: color.mainTextDim }]} numberOfLines={1} testID="order item extras">{extras?.map(item => item.name).join(", ")}</Text>
+        {extras?.length ? (
+          <Text style={[styles.itemExtras, { color: color.mainTextDim }]} numberOfLines={1} testID="order item extras">{extras?.map(item => item.name).join(", ")}</Text>
+        ) : null}
       </View>
       <Text style={[styles.totalPrice, { color: color.mainText }]} testID="order item total">{numberToCurrency(totalPrice * quantity)}</Text>
     </View>
